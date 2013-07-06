a13a18403da87c89d5077ac225a5f949a4cc8481
xwf286 <phytutor@163.com>
2013-07-06 01:43:47 -0700

    add app content

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = '1091d9db203d74b94239a07b0ae3db5baf566801b6513c0b157751ae0a72d1eb9dcb3318eb4d8bb3e588b3eec63f0a0c6382327566a89def0db68f180873d995'
